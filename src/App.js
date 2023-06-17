fd55bc0539ac4575573a710a0c8fb6abc00b3ab5
zahar <zzzyyy589@gmail.com>
2023-06-17 23:30:24 +0300

    init

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           zoom: 14
         }}
         style={{width: "100%", height: " calc(100vh - 77px)"}}
-        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=YOUR_MAPTILER_API_KEY_HERE"
+        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=sB8mHmHdWLukjiQctNE5"
       >
         <NavigationControl position="top-left" />
       </Map>
