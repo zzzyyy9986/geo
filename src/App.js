cb0f7c37f4d68315b30b389a95b7b57869527c32
zahar <zzzyyy589@gmail.com>
2023-06-18 00:43:29 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import * as React from 'react';
-import Map, {NavigationControl} from 'react-map-gl';
-import Navbar from './components/navbar.js';
-import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Map mapLib={maplibregl} 
-        initialViewState={{
-          longitude: 16.62662018,
-          latitude: 49.2125578,
-          zoom: 14
-        }}
-        style={{width: "100%", height: " calc(100vh - 77px)"}}
-        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=sB8mHmHdWLukjiQctNE5"
-      >
-        <NavigationControl position="top-left" />
-      </Map>
-    </div>
-  );
-}
+export const App = () => (
+
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+    </BrowserRouter>
+
+);
+
+const Home = () => <h2>Главная</h2>;
+
+const About = () => <h2>Контакты</h2>;
+
+const Users = () => <h2>Пользователи</h2>;
 
 export default App;
