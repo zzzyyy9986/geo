334ebf2b4e61a2476f6c0cbee57555ee04232e0c
Xjeck3 <fakenroutes@gmail.com>
2023-06-18 06:12:56 +0300

    fix3_moscowDataParametr

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
 import { MapContainer, TileLayer, useMap, FeatureGroup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -6,31 +7,129 @@ import { EditControl } from 'react-leaflet-draw';
 import 'leaflet-draw/dist/leaflet.draw.css';
 import 'leaflet-draw/dist/leaflet.draw-src.css';
 import axios from 'axios';
+import districtsData from './districtsData.json';
+import { saveAs } from 'file-saver';
+
+
 const infrastructureTypes = ["pharmacy", "kindergarten", "school", "restaurant"];
 
-function MapComponent() {
+// async function fetchDistrictBoundaries(city) {
+//   const overpassUrl = 'https://overpass-api.de/api/interpreter';
+//   const overpassQuery = `
+//     [out:json];
+//     area[name="${city}"];
+//     relation[admin_level="9"](area);
+//     out geom;
+//   `;
 
-  const DrawnItems = new L.FeatureGroup();
+//   try {
+//     const response = await axios.post(overpassUrl, `data=${encodeURIComponent(overpassQuery)}`, {
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       }
+//     });
+
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching district data: ', error);
+//     return null;
+//   }
+// }
+
+async function collectDistrictData() {
+  try {
+    const districts = districtsData.features; // используйте импортированные данные
+
+    const results = {};
+
+    for (const district of districts) {
+      const districtName = district.properties.NAME;
+
+      // Обрабатываем MultiPolygon отдельно
+      const coordinates = district.geometry.type === "MultiPolygon"
+        ? district.geometry.coordinates.flat()
+        : district.geometry.coordinates;
+
+      const boundaries = coordinates[0].map(coords => `${coords[1]} ${coords[0]}`).join(' ');
+
+      const infrastructurePromises = infrastructureTypes.map(type => fetchInfrastructureInPolygon(boundaries, type));
+      const infrastructureCounts = await axios.all(infrastructurePromises)
+        .then(axios.spread((...values) => {
+          return infrastructureTypes.reduce((result, type, index) => {
+            result[type] = values[index];
+            return result;
+          }, {});
+        }))
+        .catch(error => {
+          console.error('Error fetching infrastructure: ', error);
+          return {};
+        });
+
+      results[districtName] = infrastructureCounts;
+    }
+
+    return results;
+  } catch (error) {
+    console.error('Error collecting district data: ', error);
+  }
+}
+
+
+async function fetchDistrictData(polygon) {
+  const overpassUrl = 'https://overpass-api.de/api/interpreter';
+  const overpassQuery = `
+    [out:json];
+    (
+      relation["admin_level"="9"](poly:"${polygon}");
+    );
+    out body;
+    >;
+    out skel qt;
+  `;
+
+  try {
+    const response = await axios.post(overpassUrl, `data=${encodeURIComponent(overpassQuery)}`, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching district data: ', error);
+    return null;
+  }
+}
+
+function MapComponent({ handleCollectGeoData }) {
+  const DrawnItems = useRef(new L.FeatureGroup()).current;
   const map = useMap();
   useEffect(() => {
     DrawnItems.addTo(map);
-  }, [map]);
+  }, [map, DrawnItems]);
 
   const handleCreated = async (e) => {
-    const layer = e.layer;
-    DrawnItems.addLayer(layer);
-    const latLngs = layer.getLatLngs()[0];
-    // изменено здесь
-    const polygon = latLngs.map(latLng => `${latLng.lat} ${latLng.lng}`).join(' ');
+    try {
+      const layer = e.layer;
+      DrawnItems.addLayer(layer);
+      const latLngs = layer.getLatLngs()[0];
+      const polygon = latLngs.map(latLng => `${latLng.lat} ${latLng.lng}`).join(' ');
 
-    const infrastructureCounts = {};
-    for (const type of infrastructureTypes) {
-      const count = await fetchInfrastructureInPolygon(polygon, type);
-      infrastructureCounts[type] = count;
+      const infrastructureCounts = {};
+      for (const type of infrastructureTypes) {
+        try {
+          const count = await fetchInfrastructureInPolygon(polygon, type);
+          infrastructureCounts[type] = count;
+        } catch (error) {
+          console.error(`Error fetching infrastructure for ${type}: `, error);
+        }
+      }
+
+      console.log('Infrastructure Counts:', infrastructureCounts);
+    } catch (error) {
+      console.error('Error in handleCreated: ', error);
     }
-
-    console.log(infrastructureCounts);  // выводим результаты в консоль
-  }
+  };
 
   return (
     <FeatureGroup>
@@ -70,19 +169,52 @@ async function fetchInfrastructureInPolygon(polygon, infrastructureType) {
       }
     });
 
-    return response.data.elements.length;  // количество объектов этого типа внутри полигона
+    return response.data.elements.length;
   } catch (error) {
-    console.error('Error fetching data: ', error);
+    console.error(`Error fetching infrastructure for ${infrastructureType}: `, error);
     return null;
   }
 }
 
+
 function MyMap() {
+  const [isLoading, setIsLoading] = useState(false); // Используем isLoading для строки состояния
+  const DrawnItems = useRef(new L.FeatureGroup()).current;
+
+  const handleCollectGeoData = async () => {
+    setIsLoading(true); // Начинаем загрузку
+    try {
+      const districtData = await collectDistrictData();
+      console.log('District Data:', districtData);
+
+      // Добавляем временную метку к имени файла
+      const timeStamp = new Date().getTime();
+      const blob = new Blob([JSON.stringify(districtData)], {type: "application/json;charset=utf-8"});
+      saveAs(blob, `districtData_${timeStamp}.json`); // Сохраняем данные в файл
+
+      // Остальной ваш код...
+
+    } catch (error) {
+      console.error('Error collecting district data: ', error);
+    }
+    setIsLoading(false); // Завершаем загрузку
+  };
+
   return (
-    <MapContainer center={[55.7522200, 37.6155600]} zoom={13} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <MapComponent />
-    </MapContainer>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ flex: '2', position: 'relative' }}>
+        <MapContainer center={[55.7522200, 37.6155600]} zoom={13} style={{ height: '100%', width: '100%' }}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <MapComponent handleCollectGeoData={handleCollectGeoData} />
+        </MapContainer>
+      </div>
+      <div style={{ flex: '1', padding: '10px' }}>
+        <button onClick={handleCollectGeoData} style={{ margin: '10px 0' }}>
+          Собрать геометрические данные
+        </button>
+        {isLoading && <p>Загрузка данных...</p>} // строка состояния
+      </div>
+    </div>
   );
 }
 
